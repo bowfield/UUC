66474fdefa0c84e7f1696f5529e047b4526fd224
baddev <38311102+qbaddev@users.noreply.github.com>
2020-02-27 18:19:07 +0300

    Добавлена поддержка UTF-8
    Теперь конфиги можно заполнять русскими буквами. (utf-8)

--- src/unicon/uuc/SPrefs.java
+++ src/unicon/uuc/SPrefs.java
@@ -1,14 +1,18 @@
 package unicon.uuc;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.Properties;
 
 public class SPrefs {
     Properties p = new Properties();
     public String get(String id) throws FileNotFoundException, IOException{
-        p.load(new FileInputStream("uuc.conf"));
+        FileInputStream input = new FileInputStream(new File("uuc.conf"));
+        p.load(new InputStreamReader(input, Charset.forName("UTF-8")));
         return p.getProperty(id);
     }  
 }
