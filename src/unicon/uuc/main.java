e4b801c68b8d40a6a101e6cbfb7f5d3923b43850
baddev <38311102+qbaddev@users.noreply.github.com>
2020-02-27 20:59:22 +0300

    Добавлены комментарии

--- src/unicon/uuc/main.java
+++ src/unicon/uuc/main.java
@@ -6,10 +6,10 @@ import java.io.UnsupportedEncodingException;
 public class main {
 
     public static void main(String[] args) throws UnsupportedEncodingException, IOException{
-        String encoding = System.getProperty("console.encoding", "utf-8");
+        String encoding = System.getProperty("console.encoding", "utf-8"); // Установка кодировки
         
-        Register register = new Register();
-        new Server(register).start();
+        Register register = new Register(); // Регистрация стандартных файлов
+        new Server(register).start();       // Запуск сервера
     }
     
 }
